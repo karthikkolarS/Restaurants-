f18a9e6197f778fd3084d6fdb5b76aaa5e7b9025
karthikkolarS <karthikks44@gmail.com>
2024-04-23 00:08:23 +0200

    All data acquired and displayed in a simple web interface.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 import requests
 app = Flask(__name__)
-def fetch_restaurant_data():
+@app.route('/')
+def home():
     url = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
@@ -9,13 +10,13 @@ def fetch_restaurant_data():
     response = requests.get(url, headers=headers)
     if response.status_code == 200: #data fetch validation
         data = response.json()
-        return data.get('restaurants', [])
+        restaurants = data.get('restaurants', [])
+        for restaurant in restaurants:
+            restaurant['address'] = restaurant['address']['firstLine']
+            restaurant['cuisines'] = restaurant['cuisines'][0]['name'] 
+            restaurant['rating'] = restaurant['rating']['starRating']
+        return render_template('index.html', restaurants=restaurants[:10])
     else:
-        return []
-@app.route('/')
-def index():
-    restaurants = fetch_restaurant_data()
-    return render_template('index.html', restaurants=restaurants)
+        return "Failed to fetch restaurant data"
 if __name__ == '__main__':
     app.run()
-
